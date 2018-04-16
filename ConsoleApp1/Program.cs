5d711aadaeccbfb299ded21337f020e934087fa8
Justin Morris <PcHelpPros@Gmail.Com>
2018-04-16 10:33:59 -0400

    Working

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using MotorizedVehicles;
 using System.Threading.Tasks;
+using Structures;
 
 namespace ConsoleApp1
 {
@@ -11,7 +12,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Coupe Ford = new Coupe();
+            try
+            {
+                Coupe coupe = new Coupe();
+                coupe.Brakes = "disc";
+                coupe.EngineType = EngineType.V8;
+                coupe.NoOfTires = 1;
+
+                Automobile auto = new Automobile();
+                auto.Brakes = "disc";
+                auto.EngineType = EngineType.V8;
+
+                Coupe coupeTwin = coupe;
+                coupeTwin.EngineType = EngineType.V6;
+                Console.WriteLine($"coupe has enginetype {coupe.EngineType.ToString()}");
+
+                Automobile autoTwin = auto;
+                autoTwin.EngineType = EngineType.V6;
+                Console.WriteLine($"auto has enginetype {auto.EngineType.ToString()}");
+            }
+            catch (System.Exception e)
+            {
+                {throw e;}
+            }
             
         }
     }
